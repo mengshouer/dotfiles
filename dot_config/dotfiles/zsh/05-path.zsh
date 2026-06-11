18f0efbdd8bc1097d5284b8e93f5865169ef1c98
mengshouer <mengshouer@outlook.com>
2026-06-11 11:09:35 +0800

    refactor: adjust path precedence logic in zsh config

--- dot_config/dotfiles/zsh/05-path.zsh
+++ dot_config/dotfiles/zsh/05-path.zsh
@@ -2,10 +2,6 @@
 
 typeset -U path PATH
 
-zsh_path_prepend "$HOME/.local/bin"
-[[ -n "${XDG_BIN_HOME:-}" ]] && zsh_path_prepend "$XDG_BIN_HOME"
-[[ -n "${DOTFILES_BOOTSTRAP_BIN_DIR:-}" ]] && zsh_path_prepend "$DOTFILES_BOOTSTRAP_BIN_DIR"
-
 for brew_bin in \
   "/opt/homebrew/bin/brew" \
   "/usr/local/bin/brew" \
@@ -16,6 +12,12 @@ do
     break
   fi
 done
-
 unset brew_bin
+
+# User-local paths prepend AFTER brew so they take priority.
+zsh_path_prepend "$HOME/.local/bin"
+[[ -n "${XDG_BIN_HOME:-}" ]] && zsh_path_prepend "$XDG_BIN_HOME"
+[[ -n "${DOTFILES_BOOTSTRAP_BIN_DIR:-}" ]] && zsh_path_prepend "$DOTFILES_BOOTSTRAP_BIN_DIR"
+
 export PATH
+
