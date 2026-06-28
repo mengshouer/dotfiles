d1622529725990a52673dd6d3b79553c7df94959
mengshouer <mengshouer@outlook.com>
2026-06-29 02:04:40 +0800

    feat: enhance path management and environment setup for root and user contexts

--- dot_config/dotfiles/zsh/50-fnm.zsh
+++ dot_config/dotfiles/zsh/50-fnm.zsh
@@ -2,12 +2,24 @@
 
 zsh_init_fnm() {
   if (( ! ${+commands[fnm]} )); then
-    local fnm_dir
-    if [[ "$OSTYPE" == darwin* ]]; then
-      fnm_dir="${XDG_DATA_HOME:-$HOME/Library/Application Support}/fnm"
-    else
-      fnm_dir="${XDG_DATA_HOME:-$HOME/.local/share}/fnm"
+    local fnm_dir fnm_home
+
+    fnm_home="${dotfiles_home:-$HOME}"
+    if (( EUID == 0 && ! ${+dotfiles_home} )); then
+      fnm_home=~root
     fi
+
+    if (( EUID == 0 )); then
+      case "$OSTYPE" in
+        darwin*) fnm_dir="$fnm_home/Library/Application Support/fnm" ;;
+        *)       fnm_dir="$fnm_home/.local/share/fnm" ;;
+      esac
+    elif [[ "$OSTYPE" == darwin* ]]; then
+      fnm_dir="${XDG_DATA_HOME:-$fnm_home/Library/Application Support}/fnm"
+    else
+      fnm_dir="${XDG_DATA_HOME:-$fnm_home/.local/share}/fnm"
+    fi
+
     [[ -d "$fnm_dir" ]] && zsh_path_prepend "$fnm_dir"
     (( ${+commands[fnm]} )) || return 0
   fi
@@ -20,4 +32,3 @@ zsh_init_fnm() {
 }
 
 zsh_defer_or_run zsh_init_fnm
-
